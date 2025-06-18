72c0736bf78ec77b36c2251a6bcbb471d4ca27a7
Keerthipriya0802 <keerthipriya0802@gmail.com>
2025-06-18 16:14:20 +0530

    Time: 0 ms (100%), Space: 41.2 MB (24.29%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -6,16 +6,16 @@ class Solution {
         }
 
         while (n % 2 == 0){
-            n /= 2;
+            n =n/ 2;
         }
         while (n % 3 == 0)
         {
-            n /= 3;
+            n =n/ 3;
 
         } 
         while (n % 5 == 0) 
         {
-            n /= 5;
+            n =n/ 5;
         }
 
         return n == 1;

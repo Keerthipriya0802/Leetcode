fee4cf537c58225ac0f090f310b5ec66562f624c
Keerthipriya0802 <keerthipriya0802@gmail.com>
2025-08-21 11:40:29 +0530

    Time: 272 ms (14.8%), Space: 45.2 MB (16.48%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,25 +1,20 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        String r=a;
-        int count=1;
-        while (r.length()<b.length())
-        {
-            r=r+a;
-            count=count+1;
+        String r = a;
+        int count = 1;
+        while (r.length() < b.length()) {
+            r = r + a;
+            count = count + 1;
         }
-        if (r.contains(b))
-        {
+        if (r.contains(b)) {
             return count;
         }
-        r=r+a;
-        count=count+1;
-        if (r.contains(b))
-        {
+        r = r + a;
+        count = count + 1;
+        if (r.contains(b)) {
             return count;
         }
         return -1;
-        
 
-        
     }
 }

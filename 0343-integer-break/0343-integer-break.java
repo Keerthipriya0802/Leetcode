c47afce1dc73f3e0eeb5121966d0055bc4ae0304
Keerthipriya0802 <keerthipriya0802@gmail.com>
2025-09-06 13:35:04 +0530

    Time: 0 ms (100%), Space: 40.7 MB (30.58%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int integerBreak(int n) {
+        if(n==2)
+        {
+            return 1;
+        }
+        if(n==3)
+        {
+            return 2;
+        }
+        int flag=1;
+        while(n>4)
+        {
+            flag=flag*3;
+            n=n-3;
+
+        }
+        return flag*n;
+        
+    }
+}
